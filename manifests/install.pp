3a4716006b9ad4de51d0f0012cfc0aec7cfa876e
Tim Meusel <tim@bastelfreak.de>
2020-07-25 18:23:25 +0200

    puppet-lint: autofix

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,7 +3,6 @@
 # This class is called from jolokia for install.
 #
 class jolokia::install {
-
   package { 'jolokia-agent':
     ensure => $jolokia::jvm_agent_ensure,
     name   => $jolokia::jvm_agent_name,
